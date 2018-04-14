3a1789228a8708fcc3e09b74c77db815e973ea79
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-04-14 20:15:54 -0300

    configurado o arquivo application.properties

--- src/main/java/br/com/eventos/eventos/EventosApplication.java
+++ src/main/java/br/com/eventos/eventos/EventosApplication.java
@@ -2,11 +2,17 @@ package br.com.eventos.eventos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"br.com.eventos.controllers"})
+@EntityScan(basePackages = {"br.com.eventos.models"})
 public class EventosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventosApplication.class, args);
+
+
 	}
 }
---
+++ src/main/java/br/com/eventos/models/Evento.java
@@ -0,0 +1,58 @@
+package br.com.eventos.models;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Evento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long codigo;
+	private String nome;
+	private String local;
+	private Date data;
+	private String horario;
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	
+	
+
+}
